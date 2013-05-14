6c341a6e25b28ab4187e5d47504753deada33a08
Prashish <prashish_raj@hotmail.com>
2013-05-14 17:52:36 +0530

    dropdown and button

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
 // map english digit to nepali
 var nepDigit = ['', '१', '२', '३', '४', '५', '६', '७', '८', '९', '१०', '११', '१२', '१३', '१४', '१५', '१६', '१७', '१८', '१९', '२०', '२१', '२२', '२३', '२४', '२५', '२६', '२७', '२८', '२९', '३०', '३१', '३२'];
 
+var nepMonth = ['', 'बैशाक', 'जेष्ठ', 'असार', 'सावन', 'भाद्र', 'असोज', 'कर्तिक', 'मँसिर', 'पुस', 'माघ', 'फल्गुन', 'चैत्र'];
+
+var engMonth = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
 $(document).ready(function() {
-    for ( var i = 2000; i < 2090; i++)
-        $("#nep-year").append("<option id='" + i + "'>" + i + "</option>");
+    load();
 });
+
+function load() {
+    /*
+    for ( var i = 2000; i <= 2090; i++) {
+        $(".engyear").append("<option id='" + i + "'>" + i + "</option>");
+    }
+    */
+
+}
 
 function getCorresEngDay(date) {
     var temp = new Object();
-    temp = neptoeng.DateConversion(date, 9, 2046);
+    temp = neptoeng.DateConversion(date, 1, 2070);
     return temp.getDate();
 }
 
