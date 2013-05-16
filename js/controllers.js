c093a64d193c9f08920432eead0e1425099604d5
Prashish <prashish_raj@hotmail.com>
2013-05-16 17:32:12 +0530

    adding scopes

--- js/controllers.js
+++ js/controllers.js
@@ -3,9 +3,14 @@
 function nepaliCalendarCtrl( $scope ) {
   console.log("main ctrl");
 
+  $scope.month = $("select#nepyear").
   $scope.list1 = [ 
   {text: '1234', done:true},
   {text: '123345', done:false}];
+
+  $scope.generateCalender = new function() {
+
+  }
 }; 
 
 function nepaliCalendarCtrl2( $scope ) {
