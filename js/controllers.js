9b14f2020c060f667f09df7a7cace11eed187c8e
Prashish <prashish_raj@hotmail.com>
2013-05-16 16:33:31 +0530

    intro to angular

---
+++ js/controllers.js
@@ -0,0 +1,17 @@
+/* Controllers */
+
+function nepaliCalendarCtrl( $scope ) {
+  console.log("main ctrl");
+
+  $scope.list1 = [ 
+  {text: '1234', done:true},
+  {text: '123345', done:false}];
+}; 
+
+function nepaliCalendarCtrl2( $scope ) {
+  console.log("main ctrl2");
+
+  $scope.list1 = [ 
+  {text: '123434', done:true},
+  {text: '12332345', done:false}];
+}; 
